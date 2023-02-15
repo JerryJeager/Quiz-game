9e473e2db1c602de9016f96874b625f370346762
Amadi Jerry <amadijerry310@gmail.com>
2023-02-15 07:51:24 +0100

    fixing bugs

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ const newQuestion = document.querySelector('.javascript-questions').querySelecto
 const questionAnswers = document.querySelector('.question-answers')
 const nextQuestion = document.querySelector('.next-question-btn')
 const questionsRemaining = document.querySelector('.num-of-questions-left')
-// const correctOption = document.querySelector('.fa-circle-check')
 const time = document.querySelector('.time')
+const result = document.querySelector('.result')
+const quitBtn = document.querySelector('.quit-btn')
+const replayBtn = document.querySelector('.replay-btn')
+const totalScore = document.querySelector('.total-score')
 let score = 0
 let timeCount = 15
 
@@ -18,6 +21,7 @@ let timeCount = 15
 startBtn.addEventListener('click', () => {
     startBtn.classList.add('hide-start-btn')
     rules.classList.add('rules-display')
+    // timeCountDown()
 })
 
 exit.addEventListener('click', () => {
@@ -29,84 +33,93 @@ continueBtn.addEventListener('click', () => {
     rules.classList.remove('rules-display')
     quizQuestions.classList.add('display-quiz-questions')
     generateQuestion()
-    // timeCountDown()
-}) 
+    timeCountDown()
+})
 
 nextQuestion.addEventListener('click', () => {
-    questionAnswers.textContent = ''
-    generateQuestion()
-    // timeCount = 15
+    if (i >= 29) {
+        quizQuestions.classList.remove('display-quiz-questions')
+        result.classList.add('show-result')
+        totalScore.innerHTML = `You've completed Jerry's Quiz! and nice 🤓, You got <span>${score}</span> out of <span>30</span>`
+    }else{
+        questionAnswers.textContent = ''
+        generateQuestion()
+        // timeCountDown()
+    }
     console.log(score)
+})
+
+quitBtn.addEventListener('click', () => {
+    result.classList.remove('show-result')
+    startBtn.classList.remove('hide-start-btn')
+    i = -1
+    newQuestion.textContent = ''
+    questionAnswers.innerHTML = ''
+})
+
+replayBtn.addEventListener('click', () => {
+    result.classList.remove('show-result')
+    quizQuestions.classList.add('display-quiz-questions')
+    i = -1
+    newQuestion.textContent = ''
+    questionAnswers.innerHTML = ''
+    generateQuestion()
+    // timeCountDown()
 })
 
 let i = -1
 
 function generateQuestion() {
     i++
-    timeCount = 15
-    timeCountDown()
-    console.log(quiz[i].question)
-    console.log(newQuestion)
     newQuestion.textContent = `${i + 1}. ${quiz[i].question}`
+    timeCount = 15
     for (let j = 0; j < quiz[i].answers.length; j++) {
-        if (quiz[i].answers[j].correct == false){
+        if (quiz[i].answers[j].correct == false) {
             questionAnswers.innerHTML += `<div class="answer">
         <p>${quiz[i].answers[j].ans}</p>
         <div class="answer-status"><i class="fa-regular fa-circle-xmark"></i></div>
         </div>`
-        } else if (quiz[i].answers[j].correct == true){
+        } else if (quiz[i].answers[j].correct == true) {
             questionAnswers.innerHTML += `<div class="answer">
         <p>${quiz[i].answers[j].ans}</p>
         <div class="answer-status"><i class="fa-regular fa-circle-check"></i></div>
         </div>`
         }
-        console.log(quiz[i].answers[j].correct)
     }
     questionsRemaining.innerHTML = `<p><span>${i + 1}</span> of <span>30</span> Questions</p>`
     showAnswer()
-    if (i >= 30) {
-        return
-    }
+
 }
 
-function showAnswer(){
-    // const wrongOptions = document.querySelectorAll('.fa-circle-xmark')
-    const correctOption2 = document.querySelector('.fa-circle-check')
+function showAnswer() {
     const allAnswers = document.querySelectorAll('.answer')
     allAnswers.forEach(answer => {
         answer.addEventListener('click', () => {
-            // answer.classList.remove('disable-click')
-            handleWrongAnswers(answer, correctOption)
-            // allAnswers.classList.add('disable-click')
-        }, {once: true})
-        // answer.classList.add('disable-click')
+            if (answer.querySelector('.fa-regular').classList.contains('fa-circle-xmark') == false) {
+                answer.querySelector('.fa-circle-check').classList.add('display-status')
+                score++
+                console.log(score)
+            }
+            else {
+                answer.querySelector('.fa-circle-xmark').classList.add('display-status')
+                const correctOption = document.querySelector('.fa-circle-check')
+                correctOption.classList.add('display-status')
+            }
+        }, { once: true })
     })
-    correctOption2.addEventListener('click', () => {
-        handleCorrectOption(correctOption2)
-    }, {once: true})
-    
 }
 
-function handleWrongAnswers(answerValue, opt){
-    answerValue.querySelector('.fa-regular').classList.add('display-status')
-    opt.classList.add('display-status')
-    // answerValue.classList.add("disable-click")
-    console.log(score)
-}
-function handleCorrectOption(opt){
-    opt.classList.add('display-status')
-    ++score
-}
-
-function timeCountDown(){
+function timeCountDown() {
     setTimeout(() => {
-        time.innerHTML = `<div class="time-status">
+        if(timeCount > 0){
+            time.innerHTML = `<div class="time-status">
                         <p>Time Left</p>
                     </div>
                     <div class="time-countdown">
                         <p>${timeCount--}</p>
                     </div>`
-        if (timeCount <= 0) {
+        }
+        else if (timeCount <= 0) {
             time.innerHTML = `<div class="time-status">
                         <p>Time Off</p>
                     </div>
